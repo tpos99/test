d10da301e91e4f76f5a333a27e3330d4ab526330
tpos99 <nandatpos123@gmail.com>
2025-04-20 14:50:32 +0800

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -3,5 +3,6 @@ import yfinance as yf
 import pandas as pd
 
 def get_data(tickers, period="2y", interval="1d"):
-    data = yf.download(tickers, period=period, interval=interval)['Adj Close']
+    data = yf.download(tickers, period=period, interval=interval, group_by='ticker')
+    data = pd.DataFrame({ticker: data[ticker]['Adj Close'] for ticker in tickers if 'Adj Close' in data[ticker]})
     return data.dropna()
